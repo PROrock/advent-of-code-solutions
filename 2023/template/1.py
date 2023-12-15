df84ae7d0b7bc03b67af7e41cffe0d431a33b338
Ondrej Jelinek <ondrej.jelinek@email.cz>
2023-12-15 15:15:11 +0100

    better template

--- 2023/template/1.py
+++ 2023/template/1.py
@@ -1,15 +1,21 @@
-import sys
+from pathlib import Path
 
+
+def load_lines():
+    file = "./1.in"
+    # file = "./2.in"
+    return Path(file).read_text().splitlines()
+
+
+def process_line(line):
+    return -1
+
+
+lines = load_lines()
 s = 0
-while True:
-    line = sys.stdin.readline().rstrip("\r\n")
-    if not line:
-        break
-
-    print(line)
-
-    number = 0
-    print(number)
+for line in lines:
+    number = process_line(line)
+    # print(number)
     s += number
 
 print(s)
