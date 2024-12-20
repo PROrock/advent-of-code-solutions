deb8bc2b7159fe82aa5810b4f72f6dc4927aab61
Ondra Jelinek <ondrej.jelinek@email.cz>
2024-12-20 22:28:08 +0100

    2024 17 - 2 stars!!!

--- 2024/17_computer_ops__Chronospatial_Computer/1_with_notes.py
+++ 2024/17_computer_ops__Chronospatial_Computer/1_with_notes.py
@@ -71,11 +71,20 @@ class Computer:
             if not (operation == 3 and self.a != 0):
                 self.op_idx += 2
 
+            # part b
+            # print("o", self.output, self.program)
+            # todo
+            # if operation == 5:
+            #     if not self.check_copy():
+            #         self.output = None
+            #         return
+
     def process(self, operation, operand):
         # print(f"{operation=}, {operand=}")
         self.ops()[operation](operand)
 
     def print_state(self):
+        # print(self.a, self.b, self.c, self.op_idx)
         print(self)
 
     def check_copy(self):
@@ -91,16 +100,20 @@ program=[int(i) for i in program.split(",")]
 # print(program)
 
 a,b,c = registers
-# part a
-computer = Computer(a,b,c,program)
-computer.run()
-print(",".join([str(i) for i in computer.output]))
+# for aa in range(1, 100):  # 100_000_000 didnt finish
+#     computer = Computer(aa,b,c,program)
+#     computer.run()
+#     if computer.output is not None and computer.output == computer.program:
+#         print(aa)
+#         break
+#     # print(",".join([str(i) for i in computer.output]))
 
-# part b
+
 
 # thinking notes:
 # ops to save to a: 0,
-# pro 0,3 -> always A=floor(A/2^3) and A must be == 0 after len(program) cycles/operations
+# pro 0,3 -> vzdy A=floor(A/2^3) a to musi byt 0 po len(program) pruchodech programu = operacich
+#
 # A = 0 at the end of the last iteration
 # A = 1 - 7 at the beginning of the last iteration
 # A = 8 - 63 at the beginning of the second to last iteration
@@ -113,21 +126,71 @@ ins_codes = program[::2]
 ADV_CODE = 0  # only operation which saves something to the A reg.!
 n_advs = sum([i == ADV_CODE for i in ins_codes])
 assert n_advs == 1
+
 advs_idx = program.index(ADV_CODE)
+# advs_opers = []
+# for advs_index in advs_indices:
 advs_oper = program[advs_idx+1]
 assert advs_oper <= 3
 
+# for aa in range(1, 65):  # 100_000_000 didnt finish
+#     computer = Computer(aa,b,c,program[:-2])
+#     computer.run()
+#     print(aa, bin(aa), computer.output)
+
+
 denominator = 2**advs_oper
 n = len(program)
+# for i, program_cycle in enumerate(range(n, 0, -1)):
+#     lo = denominator ** i
+#     hi = denominator ** (i+1) -1
+#     print(i, program_cycle, lo, hi)
+
+# lo,hi = denominator ** (n-1), denominator ** n -1
+# print(lo, hi)
+# for aa in range(lo, hi+1):  # 100_000_000 didnt finish
+#     computer = Computer(aa,b,c,program)
+#     computer.run()
+#     if computer.output is not None and computer.output == computer.program:
+#         print(aa)
+#         break
+#     # print(",".join([str(i) for i in computer.output]))
+
+# for aa in range(89057279408984, 89057279408984+1):  # 100_000_000 didnt finish
+# aa = 89057279408984
+# aa = 32299373126724
+aa = 258394985014171
+computer = Computer(aa,b,c,program)
+computer.run()
+print(aa, computer.output)
+if computer.output is not None and computer.output == computer.program:
+    print("FOUND")
+    print(aa)
+
+# s = []
+# for p in program[::-1]:
+#     print(p)
+#     # s.append(bin(p))
+#     s.append(f"{p:03b}")
+# joined = "".join(reversed(s))
+# print(joined, int(joined, 2))
 
 all_candidates={}
+# candidates = list(range(8))
 candidates = [0] # so it halts!
+# for i, program_cycle in enumerate(range(n-1, -1, -1)):
+# for i, _ in enumerate(range(n-1, -1, -1)):
 for i in range(n):
+# for i, _ in enumerate(range(3, -1, -1)):
     program_cycle = len(program)-i-1
     p = program[program_cycle]
+    # p = program[program_cycle]
+    assert program[program_cycle] == program[-i-1]
     print("new", i, program_cycle, "p=", p, candidates)
 
     next_candidates = []
+    # lo = denominator ** i
+    # hi = denominator ** (i+1) -1
     for cand in candidates:
         lo = cand * denominator
         hi = (cand+1)*denominator - 1
@@ -138,13 +201,10 @@ for i in range(n):
             computer.run()
             if computer.output[0] == p and cand != aa: # to ignore zero
                 next_candidates.append(aa)
+                # print(aa, next_candidates)
 
     all_candidates[i] = next_candidates
     candidates = next_candidates
 
-aa=candidates[0]
-computer = Computer(aa,b,c,program)
-computer.run()
-if computer.output is not None and computer.output == computer.program:
-    print("RLY FOUND")
-    print(aa)
+# pprint(all_candidates)
+print(candidates)
