b98c6699b44357bbe57a7d5c6cf6d0be69c896fc
Ondra Jelinek <ondrej.jelinek@email.cz>
2024-12-21 22:24:37 +0100

    2024 18 - 2 stars

--- 2024/18_falling_bytes__RAM_run/1g.py
+++ 2024/18_falling_bytes__RAM_run/1g.py
@@ -6,8 +6,8 @@ from typing import Optional
 from utils.grid_utils import Vect, create_grid, WALL, VECTS_CLOCKWISE, inbounds, elem_at_pos, DIR_TO_VECT, inbounds_wh, \
     fill_grid, print_grid, print_grid_str
 
-# todo
-small = True
+# small = True
+small = False
 if small:
     N_FALLEN = 12
     WIDTH = HEIGHT = 7
@@ -33,8 +33,8 @@ class Node:
     prev_node: Optional["Node"]
 
     @staticmethod
-    def create(coor, score, prev_node=None):
-        return Node(State(coor), score, prev_node)
+    def create(pos, time, prev_node=None):
+        return Node(State(pos), time, prev_node)
 
     @property
     def pos(self):
@@ -46,8 +46,6 @@ class Node:
             new_pos = self.pos + dir_
             if inbounds_wh(new_pos, WIDTH, HEIGHT) and new_pos not in bytes:
                 children.append(Node.create(new_pos, self.time + 1, self))
-
-        # print(f"{self}: {children=}")
         return children
 
     def pos_path(self):
@@ -62,16 +60,13 @@ class Node:
 
 def search(start_pos, end_pos) -> Optional[Node]:
     visited_states = set()
-    start_pos = Node.create(start_pos, 0)
-    queue = deque([start_pos])
+    start_node = Node.create(start_pos, 0)
+    queue = deque([start_node])
     while queue:
         node = queue.popleft()
         if node.pos == end_pos:
-            print("Found end")
             return node
-
         if node.state in visited_states:
-            # print(f"{node.state} was already visited! {node}")
             continue
 
         queue.extend(node.expand())
@@ -79,16 +74,32 @@ def search(start_pos, end_pos) -> Optional[Node]:
     print("not found")
     return None
 
-lines = load_lines()[:N_FALLEN]
-bytes = [Vect(*[int(i) for i in l.split(",")]) for l in lines]
-print(bytes)
+lines = load_lines()
+all_bytes = [Vect(*[int(i) for i in l.split(",")]) for l in lines]
 
+bytes = all_bytes[:N_FALLEN]
+start_pos = Vect(0, 0)
+end_pos = Vect(WIDTH - 1, HEIGHT - 1)
+found_node = search(start_pos, end_pos)
+print(len(found_node.pos_path())-1)  # part a
 
-found_node = search(Vect(0, 0), Vect(WIDTH - 1, HEIGHT - 1))
-print(len(found_node.pos_path())-1)
+path_poss = set(found_node.pos_path())
+
+bytes_idx = N_FALLEN+1
+while True:
+    bytes = all_bytes[:bytes_idx]
+    # print(bytes_idx)
+    last_b = bytes[-1]
+    if last_b in path_poss:
+        found_node = search(start_pos, end_pos)
+        if found_node is None:
+            print(f"{last_b.x},{last_b.y}")
+            break
+        path_poss = set(found_node.pos_path())
+    bytes_idx += 1
+
 
 # grid = create_grid(HEIGHT, WIDTH)
 # grid = fill_grid(grid, bytes, "#")
 # grid = fill_grid(grid, found_node.pos_path())
 # print_grid_str(grid)
-
