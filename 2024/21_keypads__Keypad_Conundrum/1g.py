a5a3c16829e2043735d53a7ad79362074bc52061
Ondra Jelinek <ondrej.jelinek@email.cz>
2024-12-24 18:46:23 +0100

    2024 21 - 2 stars WIP (timing)

--- 2024/21_keypads__Keypad_Conundrum/1g.py
+++ 2024/21_keypads__Keypad_Conundrum/1g.py
@@ -1,23 +1,50 @@
 import itertools
 from functools import lru_cache
 from pathlib import Path
-from typing import Any, List
-from dataclasses import dataclass
-from utils.grid_utils import DIRS_CLOCKWISE, DIR_TO_VECT, elem_at_pos, find_one_in_grid, inbounds, PrioritizedItem, \
-    Vect, load_grid_str, ARR_TO_VECT
+from typing import NamedTuple
 
+# from utils.grid_utils import Vect, ARR_TO_VECT
+
+
+class Vect(NamedTuple):
+    x: int
+    y: int
+
+    def invert(self):
+        return Vect(-self.x, -self.y)
+
+    def __add__(self, other):
+        return Vect(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vect(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, multiplier):
+        return Vect(self.x * multiplier, self.y * multiplier)
+
+    def l1_dist(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+ARR_TO_VECT = {
+    "^": Vect(0, -1),
+    ">": Vect(1, 0),
+    "v": Vect(0, 1),
+    "<": Vect(-1, 0),
+}
 KEYPAD = {c: Vect(x,y) for y, line in enumerate(["789", "456", "123", "X0A"]) for x, c in enumerate(line)}
-# print(KEYPAD)
 DIRPAD = {c: Vect(x,y) for y, line in enumerate(["X^A", "<v>"]) for x, c in enumerate(line)}
-# print(DIRPAD)
 
 # todo
-# N_ROBOTS = 0
 # N_ROBOTS = 2
-N_ROBOTS = 5
+# N_ROBOTS = 5
 # N_ROBOTS = 10
-# N_ROBOTS = 15
+N_ROBOTS = 15
 # N_ROBOTS = 25
+
+# 5: 2747526
+#10: 261627290
+#15: 24923343972
+#
 
 def signum(x):
     if x > 0:
@@ -62,7 +89,6 @@ def to_arr(diff, pos, keypad, c):
         sym = "" if s == 0 else d_arrs[(s+1)//2]
         arrs.append(sym*abs(dim))
     # prefer x first
-    # if ">" in arrs[0] and
     # target_pos = keypad[c]
     if (pos.y == keypad["X"].y and keypad[c].x == keypad["X"].x) or (
             ">" in arrs[0] and pos.x != keypad["X"].x):
@@ -72,7 +98,7 @@ def to_arr(diff, pos, keypad, c):
 
 @lru_cache(maxsize=None)
 def spath(code, is_dirpad):
-    print("cache miss", code)
+    # print("cache miss", code)
     keypad = DIRPAD if is_dirpad else KEYPAD
     pos = keypad["A"]
     arrs = []
@@ -107,55 +133,34 @@ def spath_dirAA_WIP(code):
     return "".join(arrs)
 
 @lru_cache(maxsize=None)
-def spath_dir_rec_bad(code, n):
-    if n == 0:
-        print(n, code)
-        return code
-    # if n>=20:
-    #     print(n, code)
-
-    arrs = []
-    segments = code.split("A")[:-1]
-    # print(segments)
-    segments = [s+"A" for s in segments]
-    # print("code", code)
-    # print(segments)
-    # print(n, N_ROBOTS-n, "code", code, segments)
-
-    if n>=20:
-        print(n, code, segments)
-
-    for s in segments:
-        # arrs.extend(spath(s, is_dirpad=True))
-        next_level_code = spath(s, is_dirpad=True)
-        # next_level_code = spath_dir(s)  # doesnt make sense, already splitted into segments!
-        arrs.extend(spath_dir_rec_bad(next_level_code, n-1))
-    return "".join(arrs)
-
-@lru_cache(maxsize=None)
 def spath_dir_rec(code, n):
     if n == 0:
-        print(n, code)
+        # print(n, code)
         return code
     # if n>=20:
     #     print(n, code)
 
-    arrs = []
     segments = code.split("A")[:-1]
-    # print(segments)
     segments = [s+"A" for s in segments]
     # print("code", code)
     # print(segments)
     # print(n, N_ROBOTS-n, "code", code, segments)
 
-    if n>=20:
-        print(n, code, segments)
+    # if n>=20:
+    #     print(n, code, segments)
 
+    # 3s user 3,3 total
+    arrs = []
     for s in segments:
-        # arrs.extend(spath(s, is_dirpad=True))
-        next_level_code = spath(s, is_dirpad=True)
-        # next_level_code = spath_dir(s)  # doesnt make sense, already split into segments!
+        next_level_code = spath(s, is_dirpad=True)  # spath_dir(s) doesnt make sense, already split into segments!
         arrs.extend(spath_dir_rec(next_level_code, n-1))
+
+    # 13s user 14,3 total
+    # whole_next_code = []
+    # for s in segments:
+    #     next_level_code = spath(s, is_dirpad=True)  # spath_dir(s) doesnt make sense, already split into segments!
+    #     whole_next_code.append(next_level_code)
+    # arrs = spath_dir_rec("".join(whole_next_code), n-1)
     return "".join(arrs)
 
 
@@ -168,60 +173,27 @@ def process_line_a(line):
     arrs = spath(line, is_dirpad=False)
     arrs2 = spath(arrs, is_dirpad=True)
     arrs3 = spath(arrs2, is_dirpad=True)
-    # print(line)
-    # print("".join(arrs))
-    # print("".join(arrs2))
-    # print("".join(arrs3))
-    # exp = "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"  #3 179A
-    # exp = "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"  # 5 379A
-    # # # print(exp)
-    # # # print(line, len(arrs3), int(line[:-1]), "".join(arrs3))
     pprint(sim_whole("".join(arrs3)))
-    # print("--")
-    # pprint(sim_whole(exp))
-    # pprint3(sim_whole("".join(arrs3)))
-    # print("--")
-    # pprint3(sim_whole(exp))
-
-    #
-    # assert "".join(arrs3)==exp
-    #
     print(line, len(arrs3), int(line[:-1]), "".join(arrs3))
     return len(arrs3)*int(line[:-1])
 
-def process_line_b(line):
+def process_line_b_by_lines(line):
     arrs = spath(line, is_dirpad=False)
     levels=[arrs]
     for i in range(N_ROBOTS):
         # arrs1 = spath(arrs, is_dirpad=True)
         arrs2 = spath_dir(arrs)
-
-        # print(arrs1)
-        # print(arrs2)
-        # assert arrs1==arrs2
-        # arrs=arrs1  #226406498
-        arrs=arrs2   #226406498
+        arrs=arrs2
         levels.append(arrs)
-        print(i+1, len(arrs))
+        # print(i+1, len(arrs), "prev", len(levels[i]), "ratio", len(arrs)/len(levels[i]))  # ratio is around 2.5
 
-    # for i in range(N_ROBOTS):
-    #     # arrs1 = spath(arrs, is_dirpad=True)
-    #     arrs2 = spath_dir(arrs)
-    #
-    #     # print(arrs1)
-    #     # print(arrs2)
-    #     # assert arrs1==arrs2
-    #     # arrs=arrs1  #226406498
-    #     arrs=arrs2   #226406498
-    #     levels.append(arrs)
-    #     print(i+1, len(arrs))
-
-    pprint3(levels[::-1])
-    pprint(levels[::-1])
+    # pprint3(levels[::-1])
+    # print("-")
+    # pprint(levels[::-1])
     print(line, len(arrs), int(line[:-1]), arrs)
     return len(arrs)*int(line[:-1])
 
-def process_line_b_new(line):
+def process_line_b_rec(line):
     arrs = spath(line, is_dirpad=False)
     # levels=[arrs]
 
@@ -232,36 +204,20 @@ def process_line_b_new(line):
     for s in segments:
         arrs.extend(spath_dir_rec(s, N_ROBOTS))
 
-    # for i in range(N_ROBOTS):
-    #     # arrs1 = spath(arrs, is_dirpad=True)
-    #     arrs2 = spath_dir(arrs)
-    #
-    #     # print(arrs1)
-    #     # print(arrs2)
-    #     # assert arrs1==arrs2
-    #     # arrs=arrs1  #226406498
-    #     arrs=arrs2   #226406498
-    #     levels.append(arrs)
-    #     print(i+1, len(arrs))
-
-    # pprint3(levels[::-1])
     # pprint(levels[::-1])
     arrs = "".join(arrs)
 
-    pprint(sim_whole(arrs))
-    # pprint(sim_whole("<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"))
-    print(line, len(arrs), int(line[:-1]), arrs)
+    # pprint(sim_whole(arrs))
+    # print(line, len(arrs), int(line[:-1]), arrs)
     return len(arrs)*int(line[:-1])
-
+def process_line_b(line):
+    # return process_line_b_by_lines(line)
+    return process_line_b_rec(line)
 
 def sim_whole(arrs3):
-    # print(arrs3)
     a2 = sim(arrs3, DIRPAD["A"], DIRPAD)
     a1 = sim(a2, DIRPAD["A"], DIRPAD)
     a0 = sim(a1, KEYPAD["A"], KEYPAD)
-    # print(a2)
-    # print(a1)
-    # print(a0)
     return [arrs3, a2, a1, a0]
 def sim(arrs, pos, keypad):
     output = []
@@ -293,8 +249,6 @@ def pprint3(fas):
         print(line)
 
 
-# process_line_a("085A")
-# process_line_a("379A")
 # process_line_b("179A")
 # for d in ["283A"]:
 #     process_line_a(d)
@@ -310,8 +264,3 @@ for line in lines:
     s += number
 
 print(s)
-
-# v<A<AA>>^AvAA<^A>Av<<A>>^AAAvA^Av<A<A>>^AvA<^A>Av<A>^Av<<A>>^AAvA<^A>A
-#   v <<A>>^A<AAA>Av<A>^AvA<AA>^A
-#       <   A^^^AvA>vvA
-#           085A
