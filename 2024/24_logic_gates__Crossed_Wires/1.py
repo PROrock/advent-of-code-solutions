9dcbe58d58367cc66705824afac3ecea2c8f8cbf
Ondra Jelinek <ondrej.jelinek@email.cz>
2024-12-24 17:56:27 +0100

    2024 24 - 2 stars!!! (messy)

--- 2024/24_logic_gates__Crossed_Wires/1.py
+++ 2024/24_logic_gates__Crossed_Wires/1.py
@@ -81,31 +81,18 @@ def edge_dot(g, a):
 max_z = max(int(z[1:]) for z in zs)
 # print(max_z)
 
-correct = {}
-i=0
-out = f"z{i:02}"
-g = Gate(f"x{i:02}","XOR", f"y{i:02}", out)
-correct[out]=g
-out = f"c{i:02}"
-g = Gate(f"x{i:02}","AND", f"y{i:02}", out)
-correct[out]=g
-
-# print(correct)
-
 def add_node(out, a, op, b):
     correct[out] = Gate(a, op, b, out)
 
 def n(l, i):
     return f"{l}{i:02}"
 
-for i in range(1, max_z):
-    out = f"k{i:02}"
-    g = Gate(f"x{i:02}","XOR", f"y{i:02}", out)
-    correct[out]=g
-    out = f"l{i:02}"
-    g = Gate(f"x{i:02}","AND", f"y{i:02}", out)
-    correct[out]=g
+correct = {}
+i=0
+add_node(n("z", i), n("x", i), "XOR", n("y", i))
+add_node(n("c", i), n("x", i), "AND", n("y", i))
 
+for i in range(1, max_z):
     add_node(n("k", i), n("x", i), "XOR", n("y", i))
     add_node(n("l", i), n("x", i), "AND", n("y", i))
     add_node(n("m", i), n("c", i-1), "AND", n("k", i))
@@ -121,8 +108,6 @@ del correct[n("c", max_z-1)]
 # gen_dot(correct)
 
 assert len(correct) == len(gates)
-# print(len(correct))
-# print(len(gates))
 
 def g_by_in(gates):
     in2gates = defaultdict(list)
@@ -134,7 +119,6 @@ def g_by_in(gates):
 
 in2gates = g_by_in(gates)
 in2corrects = g_by_in(correct)
-# pprint(rev_correct)
 
 # matching!
 w2g = {}
@@ -164,7 +148,6 @@ xys=list()
 for i in range(max_z):
     for l in "xy":
         xys.append(n(l, i))
-# pprint(xys)
 
 def find_by_op(gs, op, assert_size=None):
     filtered = [g for g in gs if g.op == op]
@@ -175,17 +158,7 @@ def find_by_op(gs, op, assert_size=None):
 xys_set = set(xys)
 x_to_check = [n("x", i) for i in range(0, max_z)]
 
-
-def check_k():
-    wgs = in2gates[v]
-    cgs = in2corrects[v]
-    assert len(wgs) == len(cgs)
-    assert 1 <= len(wgs) <= 2
-    assert 1 <= len(cgs) <= 2
-    assert len(wgs) == 2
-    assert len(cgs) == 2
-    # todo acutally
-
+swapped = []
 def check_l(v: str, i, x):
     # print(f"checking {v}")
     if v.startswith("z"):
@@ -208,24 +181,22 @@ def check_l(v: str, i, x):
     # assert len(cgs) == 1
     w2g[lv]= corr_v
 
-def check_general(v: str, i, corr_v):
+def check_general(v: str, corr_v, exp_insize):
+    if corr_v == n("c", max_z-1):
+        corr_v = n("z", max_z)
+        exp_insize = 0
+
     wgs = in2gates[v]
     cgs = in2corrects[corr_v]
-    if len(wgs) != len(cgs):
-        get_candidates(wgs)
-        # gate = gates[v]
-        # swapped_with = "k" if gate.op=="XOR" else "c"
-        # print(f"candidate for swap! {v} should have {len(cgs)}, but has {len(wgs)} out arrows. Candidate for k or c node. By gate and op {gate} it is '{swapped_with}'. {wgs}")
-        # print(f"candidate for swap! {v} should have {len(cgs)}, but has {len(wgs)} out arrows. Candidate for k or c node. Wrong in2gates: {wgs}")
-        print(f"candidate for swap! {v} should have {len(cgs)} like {corr_v}, but has {len(wgs)} out arrows. Candidates {get_candidates(wgs)}. Wrong in2gates: {wgs}")
-        return
-    # assert len(wgs) == len(cgs)
-    # assert 1 <= len(wgs) <= 2
-    # assert 1 <= len(cgs) <= 2
-    # assert len(wgs) == 1
-    # assert len(cgs) == 1
-    w2g[lv]= corr_v
+    # print(v, corr_v, wgs, cgs)
+    assert exp_insize == len(cgs)
 
+    if len(wgs) != len(cgs):
+        swapped.append(v)
+        print(f"candidate for swap! {v} should have {len(cgs)} like {corr_v}, but has {len(wgs)} out arrows. Candidates {get_candidates(wgs)}. Wrong in2gates: {wgs}")
+        return None
+    w2g[lv]= corr_v
+    return corr_v
 
 def get_candidates(wgs):
     if len(wgs) == 2:
@@ -238,36 +209,57 @@ def get_candidates(wgs):
         return ["z"]
     print("WEIRD CANDICATES!!! ", wgs)
     return ["WEIRD CANDICATES!"]
-
-
+#
 for i, x in enumerate(x_to_check):
-# q=deque(xys)
-# while len(q):
-#     v = q.popleft()
     if i == 0:
         # todo
         # todo 0 as exception! doesnt have k and l but z and c!
         continue
+    # if i == 1:
+    #     # todo
+    #     # todo 0 as exception! doesnt have k and l but z and c!
+    #     continue
     v = x
     wgs = in2gates[v]
     cgs = in2corrects[v]
-    # assert len(wgs) == len(cgs)
-    # assert 1 <= len(wgs) <= 2
-    # assert 1 <= len(cgs) <= 2
-    # assert len(wgs) == 2
-    # assert len(cgs) == 2
 
-    kv = find_by_op(wgs, "XOR", 1).out
     lv = find_by_op(wgs, "AND", 1).out
-    # w2g[kv]=n("k", i)
-    # w2g[lv]=n("l", i)
-    #k
-    # check_k(kv, i, x)
-    # check_l(lv, i, x)
-    check_general(lv, i, n("l",i))
-    check_general(kv, i, n("k",i))
+    kv = find_by_op(wgs, "XOR", 1).out
+    corr_l = check_general(lv, n("l", i), 1)
+    if corr_l is not None:
+        cv = in2gates[lv][0].out
+        # check_c
+        check_general(cv, n("c", i), 2)
+
+    corr_k = check_general(kv, n("k", i), 2)
+    if corr_k is not None:
+        m_and_z = in2gates[kv]
+        # print(m_and_z)
+        mv = find_by_op(m_and_z, "AND", 1).out
+        zv = find_by_op(m_and_z, "XOR", 1).out
+        corr_m = check_general(mv, n("m", i), 1)
+        corr_z = check_general(zv, n("z", i), 0)
+# todo why 9?
 
 
+
+# inputs a,b are always right!
+# outputs can be wrong
+# network can be wrong but inputs are calling right inputs
+
+# for z in zs:
+#     wgs = in2gates[z]
+#     if len(wgs) != 0:
+#         print(f"{z} has {len(wgs)}, {wgs=}")
+# for z in zs:
+#     wgs = gates.get(z)
+#     if wgs is not None:
+#         print(f"{z} has {wgs=}")
+# # always
+
+missing = set(gates)-set(w2g)
+print(missing)
 
 
 pprint(w2g)
+print(",".join(sorted(swapped)))
