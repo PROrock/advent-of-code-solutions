a916aabeea22d4d8e1a679b4ebd6550714ce839d
Ondřej Jelínek <ondjelinek@email.cz>
2025-12-03 12:58:11 +0100

    2025 03 - 2. star

--- 2025/03_lobby/1.py
+++ 2025/03_lobby/1.py
@@ -14,12 +14,26 @@ def process_line(line):
     second = max(nums[first_i+1:])
     return first*10 + second
 
+N_BATTERIES = 12
+def process_line_part2(line):
+    nums = [int(i) for i in line]
+
+    voltage = []
+    for i in range(N_BATTERIES):
+        digit = max(nums[:len(nums)-N_BATTERIES+i+1])
+        digit_i = nums.index(digit)
+        nums = nums[digit_i+1:]
+        voltage.append(digit)
+        # print(line, i, digit, digit_i, nums)
+    return int("".join(map(str, voltage)))
+
 
 lines = load_lines()
 s = 0
 for line in lines:
-    number = process_line(line)
-    print(number, line)
+    # number = process_line(line)
+    number = process_line_part2(line)
+    # print(number, line)
     s += number
 
 print(s)
