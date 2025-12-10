e6a417236dda42432719a1f48f5e8cbcbc5c58b8
Ondřej Jelínek <ondjelinek@email.cz>
2025-12-10 17:51:47 +0100

    2025 10 1. star

--- 2025/10_/1.py
+++ 2025/10_/1.py
@@ -1,88 +1,95 @@
 import ast
+from collections import deque
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional, Any
 
 
 def load_lines():
-    file = "./1.in"
-    # file = "./2.in"
+    # file = "./1.in"
+    file = "./2.in"
     return Path(file).read_text().splitlines()
 
-
 def process_line(line):
-    target, button_sets, joltage = parse(line)
-
-
-    return -1
+    target, buttons, joltage = parse(line)
+    init_state = tuple([False for _ in range(len(target))])
+    goal_node = Search(buttons).search(init_state, target)
+    return len(goal_node.pressed_buttons)
 
 
 def parse(line):
     target_raw, *buttons_raw, joltage_raw = line.split(" ")
-    print(target_raw, buttons_raw, joltage_raw)
-    # target_nums = [m.start() for m in re.finditer(r"(#)", target_raw[1:-1])]
-    # print(target_nums)
-    # target = ["."*(len(target_raw)-2)]
-    target = [c == "#" for c in target_raw[1:-1]]
-    # print(target)
+    # print(target_raw, buttons_raw, joltage_raw)
+    target = tuple([c == "#" for c in target_raw[1:-1]])
 
     button_sets = [ast.literal_eval(b) for b in buttons_raw]
     button_sets = [(b,) if isinstance(b, int) else b for b in button_sets]
-    # list?
-    button_sets = [set(b) for b in button_sets]
-    # print(button_sets)
-
-    # buttons = [["."*len(target)] for b in button_sets]
-    # buttons = [for b, b_set in zip(buttons, button_sets)]
+    # button_sets = [set(b) for b in button_sets]
     return target, button_sets, joltage_raw
 
 @dataclass(frozen=True)
-class State:
-    state: list
-
-@dataclass(frozen=True)
 class Node:
-    # XXX: can be inlined if just one list in state
-    state: State
-    buttons: list
+    state: tuple
+    pressed_buttons: set
 
-# todo here
-#     @staticmethod
-#     def create(pos, time, prev_node=None):
-#         return Node(State(pos), time, prev_node)
-#
-#     @property
-#     def pos(self):
-#         return self.state.pos
-#
-#     def expand(self):
-#         children = []
-#         for dir_ in VECTS_CLOCKWISE:
-#             new_pos = self.pos + dir_
-#             if inbounds(grid, new_pos) and elem_at_pos(grid, new_pos) != WALL:
-#                 children.append(Node.create(new_pos, self.time + 1, self))
-#         return children
-#
-#     def node_path(self):
-#         node = self
-#         path = []
-#         while node.prev_node is not None:
-#             path.append(node)
-#             node = node.prev_node
-#         path.append(node)
-#         return list(reversed(path))
-#
-#     def pos_path(self):
-#         return [node.pos for node in self.node_path()]
-#
-#     def node_to_time(self):
-#         return {node.pos:node.time for node in self.node_path()}
+    def expand(self, buttons):
+        children = []
+        for b in buttons:
+            if b in self.pressed_buttons:
+                continue
+            # flip
+            new_state = list(self.state)
+            for pos in b:
+                new_state[pos] = not new_state[pos]
+            new_state = tuple(new_state)
+            # print(self.state, b, new_state)
+
+            children.append(Node(new_state, {*self.pressed_buttons, b}))
+        return children
+
+class Search:
+    def __init__(self, buttons):
+        self.buttons = buttons
+
+    def search(self, init_state: Any, goal_state: Any) -> Optional[Node]:
+        # print(init_state, goal_state, self.buttons)
+
+        visited_states = set()
+        queue = deque([self._get_start_node(init_state)])
+
+        while queue:
+            node = queue.popleft()
+            if node.state not in visited_states:
+                if node.state == goal_state:
+                    self.debug(f"found solution {node}")
+                    return node
+
+                new_nodes = node.expand(self.buttons)
+                queue.extend(new_nodes)
+                visited_states |= {node.state}
+
+                self.debug(node, new_nodes)
+            else:
+                self.debug(f"{node.state} already visited before!")
+
+        self.debug("goal not reached")
+        return None
+
+    @staticmethod
+    def _get_start_node(init_state) -> Node:
+        return Node(init_state, set())
+
+    @staticmethod
+    def debug(*texts):
+        pass
+        # return print(*texts, file=sys.stderr, flush=True)
 
 
 lines = load_lines()
 s = 0
 for line in lines:
     number = process_line(line)
-    # print(number)
+    print(number, line)
     s += number
 
 print(s)
