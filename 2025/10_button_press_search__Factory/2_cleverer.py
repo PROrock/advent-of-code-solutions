8c723dab8cbe1319734bae7618c7ba599cedfd8d
Ondřej Jelínek <ondjelinek@email.cz>
2025-12-12 08:50:00 +0100

    2025 10 2. star WIP :-(

--- 2025/10_button_press_search__Factory/2_cleverer.py
+++ 2025/10_button_press_search__Factory/2_cleverer.py
@@ -1,27 +1,78 @@
+from pprint import pprint
 import ast
-from collections import deque
-from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional, Any
 
 from utils.utils import ints
 
-
-# todo just copy now
 
 def load_lines():
     # file = "./1.in"
     file = "./2.in"
     return Path(file).read_text().splitlines()
 
+
+def validate(bs, cs):
+    for indices, cond_sum in cs:  # (indices, sum)
+        if sum([bs[bi] for bi in indices]) != cond_sum:
+            return False
+    return True
+
+
+def solve(buttons, joltage):
+    buttons = sorted(buttons, key=lambda b: len(b))
+    length = len(joltage)
+    max_b = [min([joltage[i] for i in b]) for b in buttons]
+    print(max_b)
+
+    buttons_with_pos = [[bi for bi, b in enumerate(buttons) if i in b] for i in range(length)]
+    pprint(buttons_with_pos)
+
+    # conditions (indices, sum)
+    cs = []
+    for i, button_with_pos in enumerate(buttons_with_pos):
+        cs.append((button_with_pos, joltage[i]))
+        sums = " + ".join([f"b{bi}*{buttons[bi]}" for bi in button_with_pos])
+        print(f"{sums}={joltage[i]}")
+    for i, mb in enumerate(max_b):
+        print(f"b{i} <= {max_b[i]}")
+
+    # brute force
+    bs = [0 for _ in range(len(buttons))]
+    bs[-1] = max_b[-1]
+
+    while True:
+        print(bs)
+
+        if validate(bs, cs):
+            print(f"found {bs}, {sum(bs)=}")
+            return sum(bs)
+
+        # next comb
+        # print("next")
+        # print(bs)
+        # todo save this adder with carry?!
+        carry = True
+        for bi in reversed(range(len(buttons))):
+            if carry:
+                if bs[bi]+1 > max_b[bi]:
+                    bs[bi] = 0
+                    carry = True
+                else:
+                    bs[bi] += 1
+                    carry = False
+                    break
+        if carry:
+            print("no solution")
+            break
+        # print(bs)
+
+    return -1
+
 def process_line(line):
     target, buttons, joltage = parse(line)
-    # init_state = tuple([0 for _ in range(len(target))])
-    init_state = [0 for _ in range(len(joltage))]
-    # goal_node = Search(buttons).search(init_state, joltage)
-    goal_node = Search(buttons).search(tuple(init_state), tuple(joltage))
-    return goal_node.pressed_buttons
+    print(buttons, joltage)
 
+    return solve(buttons, joltage)
 
 def parse(line):
     target_raw, *buttons_raw, joltage_raw = line.split(" ")
@@ -35,74 +86,13 @@ def parse(line):
     joltage = ints(joltage_raw)
     return target, button_sets, joltage
 
-@dataclass(frozen=True)
-class Node:
-    state: tuple
-    pressed_buttons: int = 0
-
-    def expand(self, buttons):
-        children = []
-        for b in buttons:
-            # if b in self.pressed_buttons:
-            #     continue
-            # flip
-            new_state = list(self.state)
-            for pos in b:
-                new_state[pos] += 1
-            new_state = tuple(new_state)
-            # print(self.state, b, new_state)
-
-            children.append(Node(new_state, self.pressed_buttons+1))
-        return children
-
-class Search:
-    def __init__(self, buttons):
-        self.buttons = buttons
-
-    def search(self, init_state: Any, goal_state: Any) -> Optional[Node]:
-        # print(init_state, goal_state, self.buttons)
-
-        visited_states = set()
-        queue = deque([self._get_start_node(init_state)])
-
-        while queue:
-            node = queue.popleft()
-            if node.state not in visited_states:
-                if node.state == goal_state:
-                    self.debug(f"found solution {node}")
-                    return node
-
-                visited_states |= {node.state}
-                # XXX: over?
-                if any(actual > goal for actual, goal in zip(node.state, goal_state)):
-                    self.debug(f"Over goal")
-                    continue
-
-                new_nodes = node.expand(self.buttons)
-                queue.extend(new_nodes)
-
-                self.debug(node, new_nodes)
-            else:
-                self.debug(f"{node.state} already visited before!")
-
-        self.debug("goal not reached")
-        return None
-
-    @staticmethod
-    def _get_start_node(init_state) -> Node:
-        return Node(init_state)
-
-    @staticmethod
-    def debug(*texts):
-        pass
-        # return print(*texts, file=sys.stderr, flush=True)
-
 
 lines = load_lines()
 s = 0
 for line in lines:
     number = process_line(line)
     print(number, line)
+    print()
     s += number
 
 print(s)
